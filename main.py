2884788a622014f63d815713df9bea04aec03cb2
FilinMen <fiilinkadabra@tutamail.com>
2024-10-09 00:47:26 +0300

    yess

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from random import randint
-
 input_data = open("input.txt","r")
 data = input_data.read()
 data = data.split()
 a = int(data[0])
 b = int(data[1])
-c = int(randint(0,11))
+c = a + b - 1
 d = c - a
 r = c - b
 t = str(d) + " " + str(r)
